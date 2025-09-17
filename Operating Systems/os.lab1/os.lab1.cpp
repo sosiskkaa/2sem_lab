edb0fb6e2ea274509a16cf9e5bddaf91280ddd27
sosiskkaa <fffjvvvh@gmail.com>
2025-09-17 17:29:52 +0300

    final correction

--- Operating Systems/os.lab1/os.lab1.cpp
+++ Operating Systems/os.lab1/os.lab1.cpp
@@ -48,8 +48,6 @@ DWORD WINAPI workerThreadCreateThread(LPVOID param)
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-
     int size;
     cout << "Enter array size: ";
     cin >> size;
@@ -108,55 +106,18 @@ int main()
         return 1;
     }
 
-    cout << "Thread created. Thread ID: " << threadID << endl;
-    cout << "Thread handle: " << hThread << endl;
-
-    cout << "Suspending thread for " << sleepTime << " ms..." << endl;
     SuspendThread(hThread);
     Sleep(sleepTime);
 
-    cout << "Resuming thread..." << endl;
     ResumeThread(hThread);
 
     WaitForSingleObject(hThread, INFINITE);
 
-    cout << "Maximum negative element: " << data.result << endl;
+    cout << "maximum negative element: " << data.result << endl;
 
     CloseHandle(hThread);
 
-    cout << "\nCreateThread demonstration (suspended thread):" << endl;
-
-    HANDLE hThread2;
-    DWORD threadID2;
-
-    ThreadData data2;
-    data2.array = array;
-    data2.size = size;
-    data2.sleepTime = sleepTime;
-    data2.result = 0;
-
-    hThread2 = CreateThread(NULL, 0, workerThreadCreateThread, &data2, CREATE_SUSPENDED, &threadID2);
-
-    if (hThread2 == NULL)
-    {
-        cout << "Second thread creation error!" << endl;
-        delete[] array;
-        return 1;
-    }
-
-    cout << "Thread created in suspended state. ID: " << threadID2 << endl;
-
-    Sleep(sleepTime);
-    cout << "Starting thread..." << endl;
-    ResumeThread(hThread2);
-
-    WaitForSingleObject(hThread2, INFINITE);
-    cout << "Maximum negative element (second thread): " << data2.result << endl;
-
-    CloseHandle(hThread2);
     delete[] array;
-
-    cout << "Program completed." << endl;
 
     return 0;
 }
